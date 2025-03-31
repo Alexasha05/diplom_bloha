88c97c9ae66b8186e8d118394330f2bf68fe2a11
Alexasha05 <73442752+Alexasha05@users.noreply.github.com>
2025-03-31 13:25:18 +0300

    Delete pag_cyc.js
    Скрипты перенесены в один файл

--- pag_cyc.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.querySelectorAll('main').forEach(main => main.style.display = 'none');
-        document.getElementById(targetId).style.display = 'block';
-    });
-});
